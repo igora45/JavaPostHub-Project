ac6aeee71ff8d8e326badbfbd8b27509f738a0d1
Igor Carvalho de Macedo Antônio <igordemacedo129@gmail.com>
2024-03-26 00:54:09 -0300

    first commit

---
+++ src/test/java/com/igorcma/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorcma.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
