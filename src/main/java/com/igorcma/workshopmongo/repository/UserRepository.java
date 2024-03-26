94d09977209616ec68a69bbda131ae2511b45db2
Igor Carvalho de Macedo Antônio <igordemacedo129@gmail.com>
2024-03-26 03:35:29 -0300

    feat: connecting to mongodb with repository and service

---
+++ src/main/java/com/igorcma/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.igorcma.workshopmongo.repository;
+
+import com.igorcma.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
