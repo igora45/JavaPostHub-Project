eebbacf45c689c2e0b9d7937c106f400725b82e0
Igor Carvalho de Macedo Antônio <igordemacedo129@gmail.com>
2024-03-30 11:14:05 -0300

    feat: User update

--- src/main/java/com/igorcma/workshopmongo/resources/UserResource.java
+++ src/main/java/com/igorcma/workshopmongo/resources/UserResource.java
@@ -32,16 +32,24 @@ public class UserResource {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
-        User obj = service.fromDto(objDto);
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        User obj = service.fromDTO(objDto);
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
-    @RequestMapping(value = "/{id}" ,method = RequestMethod.DELETE)
-    public ResponseEntity<Void> delete(@PathVariable String id){
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
         service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO objDTO){
+        User obj = service.fromDTO(objDTO);
+        obj.setId(id);
+        obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/igorcma/workshopmongo/services/UserService.java
+++ src/main/java/com/igorcma/workshopmongo/services/UserService.java
@@ -16,24 +16,39 @@ public class UserService {
     @Autowired
     private UserRepository repo;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repo.findAll();
     }
 
-    public User findById(String id){
+    public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
     }
 
-    public User insert(User obj){
+    public User insert(User obj) {
         return repo.insert(obj);
     }
 
-    public User fromDto (UserDTO objDto){
+    public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
-    public void delete(String id){
+
+    public void delete(String id) {
         User user = findById(id);
         repo.delete(user);
     }
+
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    public void updateData(User newObj, User obj) {
+        if (obj.getName() != null)
+            newObj.setName(obj.getName());
+        if (obj.getEmail() != null)
+            newObj.setEmail(obj.getEmail());
+    }
+
 }
