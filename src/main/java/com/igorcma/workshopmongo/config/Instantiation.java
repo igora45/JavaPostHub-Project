362e6fa1c5f1b91e43c61ac8de9bf6b57971f5b6
Igor Carvalho de Macedo Antônio <igordemacedo129@gmail.com>
2024-03-29 22:34:27 -0300

    feat: Using the DTO pattern to return users

--- src/main/java/com/igorcma/workshopmongo/config/Instantiation.java
+++ src/main/java/com/igorcma/workshopmongo/config/Instantiation.java
@@ -22,7 +22,8 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        User igor = new User(null, "Igor Karl", "igor@gmail.com");
 
-        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        userRepository.saveAll(Arrays.asList(maria, alex, bob, igor));
     }
 }
--- src/main/java/com/igorcma/workshopmongo/resources/UserResource.java
+++ src/main/java/com/igorcma/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.igorcma.workshopmongo.resources;
 
 import com.igorcma.workshopmongo.domain.User;
+import com.igorcma.workshopmongo.dto.UserDTO;
 import com.igorcma.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,9 +21,10 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).toList();
+        return ResponseEntity.ok().body(listDto);
     }
 
 }
